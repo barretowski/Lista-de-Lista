02b810be80174ec30caae4e633b61763381aa4c6
Paulo Henrique <paulo.barreto@microton.com.br>
2023-03-31 08:29:40 -0300

    Feat: Desenvolvimento da classe estado

--- src/Estado.java
+++ src/Estado.java
@@ -10,5 +10,36 @@ public class Estado {
     public void setInicio(NoEstado inicio){
         this.inicio = inicio;
     }
+    public void inserirEstado(String nome)
+    {
+        NoEstado aux = inicio;
+        NoEstado novo,ant;
+        novo = new NoEstado(nome,null);
+        if(aux==null)
+            this.inicio = novo;
+        else{
+            ant = aux;
+            while(aux.getProx()!=null && aux.getNome().compareToIgnoreCase(nome)>0)
+            {
+                ant = aux;
+                aux = aux.getProx();
+            }
+            if(aux.getProx()==null)
+                aux.setProx(novo);
+            else
+            {
+                ant.setProx(novo);
+                aux.setProx(aux);
+            }
+        }
+    }
+
+    public NoEstado buscarEstado(String nome){
+        NoEstado ini = this.inicio;
+        while(ini!=null && ini.getNome()!=nome)
+            ini=ini.getProx();
+
+        return ini;
+    }
 
 }
